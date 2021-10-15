b140603774a8034d771cc620768e88ed903b2ef9
Hestia-Arc <84723824+Hestia-Arc@users.noreply.github.com>
2021-10-15 09:36:07 +0100

    Create validation.js

---
+++ validation.js
@@ -0,0 +1,36 @@
+/*function validation() {
+
+	var firstN = document.getElementById("fname");
+	var lastN = document.getElementById("lname");
+	if (firstN.value == "" || lastN.value == "") {
+		alert("Names cannot be blank");
+		return false; 
+	}*/
+	const pNumber = document.getElementById("p-number");
+	const eMail = document.getElementById("email");
+	const pass = document.getElementById("pword");
+
+    const phoneRegex = /^[0-9]{3}\-[0-9]{4}\-[0-9]{4}$/;
+    const emailRegex = /^([\.\_\-a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]{2,4})$/;
+    const passRegex = /^[a-zA-Z0-9\.\,\-\_\?@\!\@\#\$\%\^\&\*\+]{5,10}$/;
+	const form = document.getElementById("myform");
+
+
+	form.addEventListener("submit", (event) => {
+
+		
+
+      if (phoneRegex.test(pNumber.value) == false) {
+
+        event.preventDefault();
+	}
+	    
+	  if (emailRegex.test(eMail.value) == false) {
+
+		event.preventDefault();
+	}
+      if (passRegex.test(pass.value) == false) {
+
+        event.preventDefault();
+	}
+	});
