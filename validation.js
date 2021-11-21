92c71358d91640078b0ed29570bcb23ec5c23a3b
Hestia-Arc <84723824+Hestia-Arc@users.noreply.github.com>
2021-11-21 13:48:12 +0100

    validcommit

--- validation.js
+++ validation.js
@@ -1,7 +1,6 @@
 
 	var http = new XMLHttpRequest();
 	var url = "https://microblog.azurewebsites.net/api/v1/Auth/register/";
-	
 	http.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 		console.log(http.responseText);
